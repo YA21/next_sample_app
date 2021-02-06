fb95dacae46dbee14b0c6f193b95344ea3d44e0c
Yuta Azumi <ytik2018e@gmail.com>
2021-02-06 22:56:02 +0900

    initial commit next.js sample application

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import App from '../components/App';
+
+const appStyle = {
+  'textAlign': 'center'
+}
+
+export default () => <div>
+  <h1>Next.js</h1>
+  <div>Welcome to next.js!</div>
+  <App/>
+</div>
